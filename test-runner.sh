479d87583278453ba1e29e752f64e65454842571
Jon Ruttan <jonruttan@gmail.com>
2020-12-27 20:46:41 -0500

    feat(test-runner.sh): use wildcard search when parameter is directory

--- test-runner.sh
+++ test-runner.sh
@@ -1,11 +1,17 @@
 SCRIPT="$0"
-TESTS="$@"
 CC=${CC:-gcc}
 INCLUDE="$(dirname "$SCRIPT")/include"
 CFLAGS=${CFLAGS:-"-Wall -Wextra -Wno-unused-parameter -fdiagnostics-color=always -g -DDEBUG -DTESTS -I$INCLUDE"}
 RUNNER=
 if [ -x "$(which valgrind)" ]; then
 	RUNNER="valgrind --leak-check=full --show-leak-kinds=all"
+fi
+
+TESTS="$@"
+
+# If $TESTS is a directory search it for tests
+if [ -d "$TESTS" ]; then
+	TESTS="$TESTS/*-test.c"
 fi
 
 # Sort by Modified date
