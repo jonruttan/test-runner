1e459388d6f63f35c4225f47e1e756972762af8c
Jon Ruttan <jonruttan@gmail.com>
2024-05-16 15:33:07 -0400

    bug(examples): add empty setup and teardown definitions

--- examples/minimal/add-integers-test.c
+++ examples/minimal/add-integers-test.c
@@ -1,6 +1,9 @@
 #include "test-runner.h"
 #include "add-integers.c"
 
+static void setup(void) {};
+static void teardown(void) {};
+
 static char *test_add_integers(void)
 {
 	_it_should("add two zero integers", add_integers(1, 2) == 3);
