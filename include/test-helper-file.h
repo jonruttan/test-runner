60b15ca1d7ff6219190f457cd61e2cdc3c4dafd3
Jon Ruttan <jonruttan@gmail.com>
2024-05-16 13:53:16 -0400

    feat(test-helper-file.h): add virtual file buffers

--- include/test-helper-file.h
+++ include/test-helper-file.h
@@ -15,10 +15,21 @@
 #define TEST_HELPER_FILE_FILE4 6
 #define TEST_HELPER_FILE_FILE5 7
 
+#define TEST_HELPER_FILE_THRESHOLD 256
+
+#define TEST_HELPER_FILE_UNDEFINED ((size_t)-1)
+
+
 char *helper_file_buffer_ptr[TEST_HELPER_FILE_DESCRIPTORS] = {
 		NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL
 	},
 	*file_buffer_ptr[TEST_HELPER_FILE_DESCRIPTORS][2];
+size_t helper_file_buffer_length[TEST_HELPER_FILE_DESCRIPTORS] = {
+		TEST_HELPER_FILE_UNDEFINED, TEST_HELPER_FILE_UNDEFINED,
+		TEST_HELPER_FILE_UNDEFINED, TEST_HELPER_FILE_UNDEFINED,
+		TEST_HELPER_FILE_UNDEFINED, TEST_HELPER_FILE_UNDEFINED,
+		TEST_HELPER_FILE_UNDEFINED, TEST_HELPER_FILE_UNDEFINED
+	};
 
 void helper_file_reset(void)
 {
@@ -41,7 +52,16 @@ char *helper_file_str(int fd)
 
 ssize_t helper_file_read(int fd, void *p_buf, size_t size)
 {
+/*	size_t max;*/
+
 	if (helper_file_buffer_ptr[fd]) {
+		if (helper_file_buffer_length[fd] != TEST_HELPER_FILE_UNDEFINED && size > helper_file_buffer_length[fd]) {
+			size = helper_file_buffer_length[fd];
+		}
+
+/*		max = strlen(helper_file_buffer_ptr[fd]);
+		size = size > max ? max : size;*/
+
 		memcpy(p_buf, file_buffer_ptr[fd][TEST_HELPER_FILE_READ], size);
 		file_buffer_ptr[fd][TEST_HELPER_FILE_READ] += size;
 	}
@@ -52,6 +72,10 @@ ssize_t helper_file_read(int fd, void *p_buf, size_t size)
 ssize_t helper_file_write(int fd, const void *p_buf, size_t size)
 {
 	if (helper_file_buffer_ptr[fd]) {
+		if (helper_file_buffer_length[fd] != TEST_HELPER_FILE_UNDEFINED && size > helper_file_buffer_length[fd]) {
+			size = helper_file_buffer_length[fd];
+		}
+
 		memcpy(file_buffer_ptr[fd][TEST_HELPER_FILE_WRITE], p_buf, size);
 		file_buffer_ptr[fd][TEST_HELPER_FILE_WRITE] += size;
 	}
